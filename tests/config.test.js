d12d4532fba2e8c49db782f67e5ffdab55c7104c
David SPORN <sporniket.studio@gmail.com>
2025-03-15 18:49:45 +0100

    [npm] added jest and some test

---
+++ tests/config.test.js
@@ -0,0 +1,36 @@
+const config = require("../lib/config.js")
+
+/* SPDX-License-Identifier: GPL-3.0-or-later */
+/****************************************
+
+---
+Copyright (C) 2025 David SPORN
+---
+This is part of **build-cmake-for-pulsar**.
+Generate and build cmake projects from within Pulsar.
+****************************************/
+
+test('loadConfigurationMap should map keys and load them', () => {
+    expect(config.loadConfigurationMap(["a", "b", "c"], (k) => `value of ${k}`, (k) => `prefix.${k}.suffix`))
+        .toEqual(
+            new Map([
+                ["a", new Map([
+                    ["key", "prefix.a.suffix"],
+                    ["value", "value of prefix.a.suffix"]
+                ])],
+                ["b", new Map([
+                    ["key", "prefix.b.suffix"],
+                    ["value", "value of prefix.b.suffix"]
+                ])],
+                ["c", new Map([
+                    ["key", "prefix.c.suffix"],
+                    ["value", "value of prefix.c.suffix"]
+                ])]
+            ])
+        );
+})
+
+test('loadConfigurationMap should map empty list of keys to empty map', () => {
+    expect(config.loadConfigurationMap([], (k) => `value of ${k}`, (k) => `prefix.${k}.suffix`))
+        .toEqual(new Map());
+})
